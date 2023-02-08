818e91bd04afe813d691bf7b7d8834754eb37515
Ginni Pinckert <gcpinckert@gmail.com>
2023-02-08 15:15:19 -0800

    Change to work with React frontend

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const express = require('express');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8000 });
 
-wss.on('connection', (ws) => {
+const app = express();
+const wsServ = new WebSocket.Server({ noServer: true });
+
+app.use(express.static('dist'));
+
+wsServ.on('connection', (ws) => {
   console.log("Client has connected");
   ws.on('message', (data, isBinary) => {
-    wss.clients.forEach(client => {
+    wsServ.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
       }
     });
+  });
+});
+
+const server = app.listen(8000);
+server.on('upgrade', (req, ws, head) => {
+  wsServ.handleUpgrade(req, ws, head, (ws) => {
+    wsServ.emit('connection', ws, req);
   });
 });
 
